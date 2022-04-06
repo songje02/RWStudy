3b77e8243a525ff5843a3ef27cb3a11520ddc1ff
송지은 <songje1005@naver.com>
2022-04-06 12:34:02 +0900

    [6주차] 반응형웹 과제

---
+++ Lecture 06/script.js
@@ -0,0 +1,11 @@
+function Init(){
+    var co = document.getElementsByClassName("co");
+    co[0].style.display = 'block';
+    co[1].style.display = 'block';
+    co[2].style.display = 'block';
+    co[3].style.display = 'block';
+    co[4].style.display = 'block';
+    co[5].style.display = 'block';
+}
+
+    
